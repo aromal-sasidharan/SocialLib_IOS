332c766ba580586b053cabf146d00c139ec9df15
aromal <aromalsasidharan@gmail.com>
2014-01-08 15:54:34 +0530

    Shared Feature added

--- SocialLibrary/SocialConnectLibrary/TwitterLib/TwitterShareMediaRequest.h
+++ SocialLibrary/SocialConnectLibrary/TwitterLib/TwitterShareMediaRequest.h
@@ -8,8 +8,11 @@
 
 #import "TwitterBaseRequest.h"
 #import "TwitterConstants.h"
-@interface TwitterShareMediaRequest : TwitterBaseRequest
+@interface TwitterShareMediaRequest : TWRequest
 -(id)init;
+@property(strong,nonatomic) NSString* type;
+@property(strong,nonatomic)NSMutableDictionary* params;
+@property(strong,nonatomic)id<TwitterResultantDelegate> twitterResultantDelegate;;
 -(void)addImageData:(NSData*)data;
 -(void)addStatus:(NSString*)status;
 @end
--- SocialLibrary/SocialConnectLibrary/TwitterLib/TwitterShareMediaRequest.m
+++ SocialLibrary/SocialConnectLibrary/TwitterLib/TwitterShareMediaRequest.m
@@ -9,10 +9,11 @@
 #import "TwitterShareMediaRequest.h"
 
 @implementation TwitterShareMediaRequest
+@synthesize type,twitterResultantDelegate;
 -(id)init
 {
-    self = [super initWithUrl:TWITTER_SHARE_MEDIA_URL];
-    [self setMethodToGet];
+    self = [super initWithURL:[NSURL URLWithString:TWITTER_SHARE_MEDIA_URL] parameters:NULL requestMethod:TWRequestMethodPOST];
+    
     self.type = @"TwitterShareMediaRequest";
     return self;
 }
@@ -22,9 +23,96 @@
 
     //add image
     [self addMultiPartData:data withName:@"media" type:@"multipart/form-data"];
+    
 }
 -(void)addStatus:(NSString*)status
 {
     [self addMultiPartData:[status dataUsingEncoding:NSUTF8StringEncoding] withName:@"status" type:@"multipart/form-data"];
+}
+
+-(void)executeRequest
+{
+    [self grandAccess];
+}
+-(void)executeRequestWithAccount:(ACAccount*)account
+{
+    
+
+    
+    [self setAccount:account];
+    
+    
+    
+    [self performRequestWithHandler:
+     
+     ^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+         if([self.twitterResultantDelegate respondsToSelector:@selector(twitterRequestDidRecieveResult:WithResponseCode:andUTF8String:andType:)])
+         {
+             
+             NSString *responseBody = [[NSString alloc] initWithData:responseData
+                                                            encoding:NSUTF8StringEncoding];
+             [self.twitterResultantDelegate twitterRequestDidRecieveResult:responseData  WithResponseCode:[urlResponse statusCode] andUTF8String:responseBody andType:self.type];
+         }
+         
+         if([self.twitterResultantDelegate respondsToSelector:@selector(twitterRequestDidRecieveResult:andUTF8String:andType:)])
+         {
+             
+             NSString *responseBody = [[NSString alloc] initWithData:responseData
+                                                            encoding:NSUTF8StringEncoding];
+             [self.twitterResultantDelegate twitterRequestDidRecieveResult:responseData andUTF8String:responseBody andType:self.type];
+         }
+         
+         if([self.twitterResultantDelegate respondsToSelector:@selector(twitterRequestDidRecieveError:andType:)])
+         {
+             [self.twitterResultantDelegate twitterRequestDidRecieveError:error andType:self.type];
+         }
+     }];
+    
+}
+
+-(void)grandAccess
+{
+    
+    ACAccountStore *store = [[ACAccountStore alloc] init];
+    
+    ACAccountType *twitterAccountType =
+    
+    [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    
+    
+    //  Request permission from the user to access the available Twitter accounts
+    
+    [store requestAccessToAccountsWithType:twitterAccountType
+     
+                     withCompletionHandler:^(BOOL granted, NSError *error) {
+                         
+                         if (!granted) {
+                             
+                             NSLog(@"user access not granded");
+                             
+                         }
+                         
+                         else {
+                             NSArray *twitterAccounts =
+                             
+                             [store accountsWithAccountType:twitterAccountType];
+                             
+                             
+                             
+                             if ([twitterAccounts count] > 0) {
+                                 
+                                 // Use the first account for simplicity
+                                 
+                                 ACAccount *account = [twitterAccounts objectAtIndex:0];
+                                 [self executeRequestWithAccount:account];
+                             }
+                             // Grab the available accounts
+                         }}];
+}
+
+-(NSMutableDictionary*) getPostParams
+{
+    return self.params;
 }
 @end
